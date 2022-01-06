2155943f443cbf91f635ccec3bfdba598c4e78d6
Rodney Richardson <rodney.richardson@cambridgeconsultants.com>
2022-01-06 15:13:39 +0000

    Fix (or explicitly ignore) xUnit warnings.

--- UnitTest/DataStructuresTests/BTreeTest.cs
+++ UnitTest/DataStructuresTests/BTreeTest.cs
@@ -171,7 +171,7 @@ namespace UnitTest.DataStructuresTests
              */
 
             // The tree should now be rooted around 40, with the left child full.
-            Assert.Equal(null, bTree.Search(5));
+            Assert.Null(bTree.Search(5));
             Assert.Equal(2, bTree.Root.Children.Count);
             Assert.Equal(7, bTree.Root.Children[0].Keys.Count); // left-most
             Assert.Equal(3, bTree.Root.Children[1].Keys.Count); // right-most
